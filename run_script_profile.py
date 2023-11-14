1e42bac36a75b7eea2990622051c600f6aa6e51e
MaxBlesch <maximilian.blesch@hu-berlin.de>
2023-11-14 11:24:38 -0500

    Done with profiling.

--- run_script_profile.py
+++ run_script_profile.py
@@ -1,9 +1,11 @@
 import sys
+
 sys.path.insert(0, "submodules/dc-egm/src/")
 import numpy as np
 import jax.numpy as jnp
-import jax
 from dcegm.solve import get_solve_function
+
+
 start_age = 25
 end_age = 75
 n_periods = end_age - start_age + 1
@@ -17,13 +19,12 @@ n_possible_retirement_ages = max_retirement_age - min_retirement_age + 1
 # when you are (start_age) years old, there can be as many policy states as there are years until (resolution_age)
 n_possible_policy_states = resolution_age - start_age + 1
 # choices: 0 = unemployment, , 1 = work, 2 = retire
-choices = np.array([0,1,2])
+choices = np.array([0, 1, 2])
 
 options_test = {
-
     "state_space": {
         "n_periods": n_periods,
-        "choices": np.array([0,1,2]),
+        "choices": np.array([0, 1, 2]),
         "endogenous_states": {
             "experience": np.arange(n_periods, dtype=int),
             "policy_state": np.arange(n_possible_policy_states, dtype=int),
@@ -51,21 +52,21 @@ options_test = {
         "gamma_0": 10,
         "gamma_1": 1,
         "gamma_2": -0.1,
-    }
-        }
+    },
+}
 
 params_dict_test = {
-    "mu": 0.5, # Risk aversion
-    "delta": 4, # Disutility of work
+    "mu": 0.5,  # Risk aversion
+    "delta": 4,  # Disutility of work
     "interest_rate": 0.03,
-    "lambda": 1e-16, # Taste shock scale/variance. Almost equal zero = no taste shocks
-    "beta": 0.95, # Discount factor
-    "sigma": 1 # Income shock scale/variance.
+    "lambda": 1e-16,  # Taste shock scale/variance. Almost equal zero = no taste shocks
+    "beta": 0.95,  # Discount factor
+    "sigma": 1,  # Income shock scale/variance.
 }
 
 
 def sparsity_condition(
-        period, lagged_choice, policy_state, retirement_age_id, experience, options
+    period, lagged_choice, policy_state, retirement_age_id, experience, options
 ):
     min_ret_age = options["min_retirement_age"]
     start_age = options["start_age"]
@@ -104,8 +105,7 @@ def sparsity_condition(
 
 
 def update_state_space(
-        period, choice, lagged_choice, policy_state, retirement_age_id, experience,
-        options
+    period, choice, lagged_choice, policy_state, retirement_age_id, experience, options
 ):
     next_state = dict()
 
@@ -132,8 +132,10 @@ def update_state_space(
 
 def state_specific_choice_set(period, lagged_choice, policy_state, options):
     age = period + options["start_age"]
-    min_individual_retirement_age = options["min_retirement_age"] + policy_state * \
-                                    options["belief_update_increment"]
+    min_individual_retirement_age = (
+        options["min_retirement_age"]
+        + policy_state * options["belief_update_increment"]
+    )
 
     if age < min_individual_retirement_age:
         return np.array([0, 1])
@@ -144,31 +146,34 @@ def state_specific_choice_set(period, lagged_choice, policy_state, options):
     else:
         return np.array([0, 1, 2])
 
+
 state_space_functions = {
     "update_endog_state_by_state_and_choice": update_state_space,
     "get_state_specific_choice_set": state_specific_choice_set,
 }
 
 # put sparsity condition into endogenous states dict within options
-options_test["state_space"]["endogenous_states"]["sparsity_condition"] = sparsity_condition
+options_test["state_space"]["endogenous_states"][
+    "sparsity_condition"
+] = sparsity_condition
 
 
-def utility_func(consumption, choice, params_dict):
-    mu = params_dict["mu"]
-    delta = params_dict["delta"]
+def utility_func(consumption, choice, params):
+    mu = params["mu"]
+    delta = params["delta"]
     is_working = choice == 1
     utility = consumption ** (1 - mu) / (1 - mu) - delta * is_working
     return utility
 
 
-def marg_utility(consumption, params_dict):
-    mu = params_dict["mu"]
-    marg_util = consumption ** -mu
+def marg_utility(consumption, params):
+    mu = params["mu"]
+    marg_util = consumption**-mu
     return marg_util
 
 
-def inverse_marginal(marginal_utility, params_dict):
-    mu = params_dict["mu"]
+def inverse_marginal(marginal_utility, params):
+    mu = params["mu"]
     return marginal_utility ** (-1 / mu)
 
 
@@ -180,12 +185,12 @@ utility_functions = {
 
 
 def solve_final_period_scalar(
-        choice,
-        begin_of_period_resources,
-        params,
-        options,
-        compute_utility,
-        compute_marginal_utility,
+    choice,
+    begin_of_period_resources,
+    params,
+    options,
+    compute_utility,
+    compute_marginal_utility,
 ):
     """Compute optimal consumption policy and value function in the final period.
 
@@ -214,24 +219,27 @@ def solve_final_period_scalar(
     consumption = begin_of_period_resources
 
     # utility & marginal utility of eating everything
-    value = compute_utility(consumption=begin_of_period_resources, choice=choice,
-                            params_dict=params)
+    value = compute_utility(
+        consumption=begin_of_period_resources, choice=choice, params=params
+    )
 
     marginal_utility = compute_marginal_utility(
-        consumption=begin_of_period_resources, params_dict=params
+        consumption=begin_of_period_resources, params=params
     )
 
     return marginal_utility, value, consumption
 
 
-def budget_constraint(lagged_choice,  # d_{t-1}
-                      experience,
-                      policy_state,  # current applicable SRA identifyer
-                      retirement_age_id,
-                      savings_end_of_previous_period,  # A_{t-1}
-                      income_shock_previous_period,  # epsilon_{t - 1}
-                      params,
-                      options):
+def budget_constraint(
+    lagged_choice,  # d_{t-1}
+    experience,
+    policy_state,  # current applicable SRA identifyer
+    retirement_age_id,
+    savings_end_of_previous_period,  # A_{t-1}
+    income_shock_previous_period,  # epsilon_{t - 1}
+    params,
+    options,
+):
     # fetch necessary parameters (gammas for wage, pension_point_value & ERP for pension)
     gamma_0 = options["gamma_0"]
     gamma_1 = options["gamma_1"]
@@ -240,8 +248,9 @@ def budget_constraint(lagged_choice,  # d_{t-1}
     ERP = options["early_retirement_penalty"]
 
     # generate actual retirement age and SRA at resolution
-    SRA_at_resolution = options["mininum_SRA"] + policy_state * options[
-        "belief_update_increment"]
+    SRA_at_resolution = (
+        options["minimum_SRA"] + policy_state * options["belief_update_increment"]
+    )
     actual_retirement_age = options["min_retirement_age"] + retirement_age_id
 
     # calculate applicable SRA and pension deduction/increase factor
@@ -256,10 +265,19 @@ def budget_constraint(lagged_choice,  # d_{t-1}
 
     # decision-specific income
     unemployment_benefits = options["unemployment_benefits"]
-    labor_income = gamma_0 + gamma_1 * experience + gamma_2 * experience ** 2 + income_shock_previous_period
+    labor_income = (
+        gamma_0
+        + gamma_1 * experience
+        + gamma_2 * experience**2
+        + income_shock_previous_period
+    )
     retirement_income = pension_point_value * experience * pension_factor
 
-    income = is_unemployed * unemployment_benefits + is_worker * labor_income + is_retired * retirement_income
+    income = (
+        is_unemployed * unemployment_benefits
+        + is_worker * labor_income
+        + is_retired * retirement_income
+    )
 
     # calculate beginning of period wealth M_t
     wealth = (1 + params["interest_rate"]) * savings_end_of_previous_period + income
@@ -267,12 +285,36 @@ def budget_constraint(lagged_choice,  # d_{t-1}
     return wealth
 
 
-savings_grid=jnp.arange(start=0,stop=100,step=0.5)
+savings_grid = jnp.arange(start=0, stop=100, step=0.5)
 
-solve_func = get_solve_function(options=options_test,
-                  exog_savings_grid=savings_grid,
-                  utility_functions=utility_functions,
-                  budget_constraint=budget_constraint,
-                  state_space_functions=state_space_functions,
-                  final_period_solution=solve_final_period_scalar,
-                  )
+import time
+
+start_pre = time.time()
+solve_func = get_solve_function(
+    options=options_test,
+    exog_savings_grid=savings_grid,
+    utility_functions=utility_functions,
+    budget_constraint=budget_constraint,
+    state_space_functions=state_space_functions,
+    final_period_solution=solve_final_period_scalar,
+)
+end_pre = time.time()
+print("Preprocessing time: ", end_pre - start_pre)
+
+solve_func(params_dict_test)
+end_first = time.time()
+print("First time_call: ", end_first - end_pre)
+
+solve_func(params_dict_test)
+end_second = time.time()
+print("Second time_call: ", end_second - end_first)
+
+
+f = open("runtime.txt", "w")
+lines_to_write = [
+    f"Pre processing time on max machine was {end_pre - start_pre} " f"seconds \n",
+    f"First time_call was {end_first - end_pre} seconds \n",
+    f"Second time_call was {end_second - end_first} seconds \n",
+]
+f.writelines(lines_to_write)
+f.close()
